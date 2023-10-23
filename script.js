657ae770bb5fbcbc33e60c2473c09ee2005fe99a
Lucas <luke.babers@icloud.com>
2023-10-23 17:32:43 -0500

    Adds the script for the Module 5 assignment

--- script.js
+++ script.js
@@ -2,22 +2,83 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  // Get the current date using Day.js
+var currentDate = dayjs().format("dddd, MMMM D, YYYY");
+
+document.getElementById("currentDay").textContent = currentDate;
+
+
+// Get the current hour using Day.js
+var currentHour = dayjs().hour();
+
+
+//Makes the hours only between 9AM-5PM appear, and applies the color coding to each box.
+for (var hour = 9; hour <= 17; hour++) {
+
+  //If statement that applies the class to each time box
+  var timeBlock = $('<div>').attr('id','hour-' + hour).addClass('row time-block');
+    if (hour < currentHour) {
+      timeBlock.addClass("past")
+    }
+      else if (hour === currentHour) {
+        timeBlock.addClass("present")
+      }
+      else {
+        timeBlock.addClass('future')
+      }
+  
+  //If statement that displays the times for each time box
+  var displayHour;
+      if (hour > 12) {
+        displayHour = hour - 12 + "PM";
+      }
+      else {
+       displayHour = hour + "AM";
+      }
+
+  //creates the duv for the time display
+  var hourDiv = $('<div>').addClass('col-2 col-md-1 hour text-center py-3').text(displayHour);
+
+  //creates the textbox
+  var textarea = $('<textarea>').addClass("col-8 col-md-10 description").attr('rows', 3);
+
+  //creates the save button
+  var saveButton = $('<button>').addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save').html('<i class="fas fa-save" aria-hidden="true"></i>').attr('id', hour).on('click', function(e){
+    saveText(this.getAttribute('id'))
+  })
+
+  //Puts the hourDiv, textarea, saveButton within the timeBlock div
+  timeBlock.append(hourDiv);
+  timeBlock.append(textarea);
+  timeBlock.append(saveButton);
+
+  $(".container-lg").append(timeBlock);
+}
+
+// Function to save the text input into local storage
+function saveText(hour) {
+  var textarea = document.getElementById("hour-" + hour).querySelector("textarea");
+  var text = textarea.value.trim();
+
+  // If statement that checks if the text is not empty
+  if (text !== "") {
+    localStorage.setItem("hour-" + hour, text);
+  }
+}
+
+// Function to load the saved text from local storage and display it on the page
+function loadSavedText() {
+  for (var hour = 9; hour <= 17; hour++) {
+    var text = localStorage.getItem("hour-" + hour);
+    var textarea = document.getElementById("hour-" + hour).querySelector("textarea");
+
+    if (text) {
+      textarea.value = text;
+    }
+  }
+}
+
+// Call the function to load the saved text when the page loads
+loadSavedText();
 });
